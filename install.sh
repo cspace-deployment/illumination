159ca5ba9a6fa6eebea8c6eed32c368221402b04
John Lowe <jblowe@berkeley.edu>
2017-11-05 07:01:40 -0800

    PAHMA-1598: minor refactoring of install script; add a revised template

--- install.sh
+++ install.sh
@@ -10,42 +10,48 @@ fi
 cd
 rails new ${app_name} -m https://raw.github.com/projectblacklight/blacklight/master/template.demo.rb
 
+source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${app_name}
 
-cp ${source_dir}/Gemfile Gemfile 
-cp ${source_dir}/blacklight.yml config/blacklight.yml 
-cp ${source_dir}/blacklight.en.yml config/locales/blacklight.en.yml
-cp ${source_dir}/development.rb config/environments/development.rb
-cp ${source_dir}/catalog_controller.rb app/controllers/catalog_controller.rb
+cp ${source_dir}/Gemfile .
+cp ${source_dir}/blacklight.yml config/
+cp ${source_dir}/blacklight.en.yml config/locales/
+cp ${source_dir}/development.rb config/environments/
+cp ${source_dir}/catalog_controller.rb app/controllers/
 mkdir -p app/helpers/blacklight
-cp ${source_dir}/catalog_helper_behavior.rb app/helpers/blacklight/catalog_helper_behavior.rb 
+cp ${source_dir}/catalog_helper_behavior.rb app/helpers/blacklight/
 
+bundle update
+# stop the troublesome spring server, for now
+bin/spring stop
+
+rails g blacklight_advanced_search:install
+rails g blacklight_gallery:install
+
+# additional customization of templates and css
 cp ${source_dir}/*.svg public/
 cp ${source_dir}/*.png public/
 mkdir -p app/views/shared
-cp ${source_dir}/_header_navbar.html.erb app/views/shared/_header_navbar.html.erb
+cp ${source_dir}/_header_navbar.html.erb app/views/shared/
 cp ${source_dir}/_footer.html.erb app/views/shared/
 mkdir -p app/views/catalog/
-cp ${source_dir}/_home_text.html.erb app/views/catalog/_home_text.html.erb
+cp ${source_dir}/_home_text.html.erb app/views/catalog/
+cp ${source_dir}/_search_form.html.erb app/views/catalog/
 
 cp ${source_dir}/_variables.scss app/assets/stylesheets/
 cp ${source_dir}/blacklight.scss app/assets/stylesheets/
 
+# a useful script for saving back modifications to source repo
 cp ${source_dir}/save.sh .
-
-bundle update
-
-rails g blacklight_advanced_search:install
-rails g blacklight_gallery:install
 
 echo
 echo "********************************************************************"
 echo "new blacklight app customized for ${tenant} created in ~/${app_name}."
 echo "to start it up in development:"
 echo ""
-echo "    cd ~/${app_name}
+echo "    cd ${app_name}"
 echo "    rails s"
 echo ""
-echo "then visit https://localhost:3000" to test"
+echo "then visit https://localhost:3000 to test"
 echo "********************************************************************"
 
