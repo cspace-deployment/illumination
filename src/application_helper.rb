660d3392a4172f506f9ba522a65097fbb2a5a4ab
John Lowe <jblowe@berkeley.edu>
2018-01-27 15:16:48 -0800

    PAHMA-1610: make wrap images in show display with href to originaljpeg

--- src/application_helper.rb
+++ src/application_helper.rb
@@ -1,14 +1,20 @@
 module ApplicationHelper
 
-  def render_csid csid
-      "https://webapps.cspace.berkeley.edu/pahma/imageserver/blobs/#{csid}/derivatives/Thumbnail/content"
+  def render_csid csid, derivative
+      "https://webapps.cspace.berkeley.edu/pahma/imageserver/blobs/#{csid}/derivatives/#{derivative}/content"
   end
 
   def render_media options={}
     # return a list of cards or images
     content_tag(:div) do
       options[:value].collect do |blob_csid|
-        content_tag(:img, '', src: render_csid(blob_csid), style: 'padding: 3px;', class: 'someclass')
+         content_tag(:a, content_tag(:img, '',
+           src: render_csid(blob_csid, 'Thumbnail'),
+           class: 'thumbclass'),
+           href: render_csid(blob_csid, 'OriginalJpeg'),
+           target: 'originaljpeg',
+           style: 'padding: 3px;',
+           class: 'hrefclass')
       end.join.html_safe
     end
   end
