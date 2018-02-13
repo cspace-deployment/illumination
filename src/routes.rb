e7ffca61adc77fa329c649e721c2f26ff3f9a15c
John Lowe <jblowe@berkeley.edu>
2018-02-12 21:09:41 -0800

    PAHMA-1610: further advanced search experimentation

--- src/routes.rb
+++ src/routes.rb
@@ -30,8 +30,14 @@ Rails.application.routes.draw do
   end
 
   # hack to get advanced search to at least start
+
+  # https://groups.google.com/d/msg/blacklight-development/Lps2C8DYj0c/q2pvEtmyAwAJ
   get 'advanced' => 'advanced#index', as: 'advanced_search'
   match 'advanced/range_limit', :to => 'advanced#range_limit', :as => 'catalog_range_limit', :via => [:get, :post]
+
+  # https://github.com/projectblacklight/blacklight_range_limit/issues/58
+  #get 'advanced' => 'advanced#index'
+  #get 'advanced/range_limit' => 'advanced#range_limit'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
